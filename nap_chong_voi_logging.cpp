365ecdd90f417fa52df98badbb8a4bcf895a7f50
Phạm Đỗ Anh <phamdoanh08082004@outlook.com.vn>
2023-12-05 17:12:42 +0700

    update

--- nap_chong_voi_logging.cpp
+++ nap_chong_voi_logging.cpp
@@ -1,93 +1,37 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <ctime>
+#include<string>
 
 using namespace std;
 
-class GhiLog {
+class Logger {
 public:
-    GhiLog(const string& tenTepTin) {
-        tepTinLog.open(tenTepTin, ios::out | ios::app);
-        if (!tepTinLog.is_open()) {
-            cerr << "Loi mo tep tin log: " << tenTepTin << endl;
-        }
-    }
-
-    ~GhiLog() {
-        if (tepTinLog.is_open()) {
-            tepTinLog.close();
-        }
-    }
-
-    void ghiLog(const string& thongDiep) {
-        ghiLog("LOG", thongDiep);
-    }
-
-    void ghiDebug(const string& thongDiep) {
-        ghiLog("DEBUG", thongDiep);
-    }
-
-    void ghiThongTin(const string& thongDiep) {
-        ghiLog("THONG_TIN", thongDiep);
-    }
-
-    void ghiLoi(const string& thongDiep) {
-        ghiLog("LOI", thongDiep);
-    }
-
-private:
-    ofstream tepTinLog;
-
-    void ghiLog(const string& capDo, const string& thongDiep) {
-        time_t thoiGianHienTai = time(0);
-        tm* thongTinThoiGian = localtime(&thoiGianHienTai);
-        char timestamp[80];
-        strftime(timestamp, sizeof(timestamp), "[%Y-%m-%d %H:%M:%S] ", thongTinThoiGian);
-
-        cout << timestamp << "[" << capDo << "] " << thongDiep << endl;
-
-        if (tepTinLog.is_open()) {
-            tepTinLog << timestamp << "[" << capDo << "] " << thongDiep << endl;
-            tepTinLog.flush(); // Dam bao ghi vao tep tin ngay lap tuc
+    static void log(const string& message, int check) {
+        if(check==0){
+            cout <<"[INFO]: "<< message << endl;
+        }else if(check==1){
+            cout <<"[DEBUG]: "<< message << endl;
+        }else if(check==2){
+            cout <<"[ERROR]: "<< message << endl;
         }
     }
 };
 
-class MaTran {
+class Matrix {
 private:
-    int m; // so hang
-    int n; // so cot
+    int m; 
+    int n; 
     double elements[100][100];
 
 public:
-    MaTran() {
-        m = 0;
-        n = 0;
-    }
+    Matrix() : m(0), n(0) {}
 
-    MaTran operator+(const MaTran& a) const {
-        GhiLog logger("logfile.txt");
-        logger.ghiLog("Thuc hien phep cong ma tran.");
-        return performOperation(a, "+");
-    }
-
-    MaTran operator-(const MaTran& a) const {
-        GhiLog logger("logfile.txt");
-        logger.ghiLog("Thuc hien phep tru ma tran.");
-        return performOperation(a, "-");
-    }
-
-    MaTran operator*(const MaTran& a) const {
-        GhiLog logger("logfile.txt");
-        logger.ghiLog("Thuc hien phep nhan ma tran.");
-        return performOperation(a, "*");
-    }
-
-    MaTran operator/(const MaTran& a) const {
-        GhiLog logger("logfile.txt");
-        logger.ghiLog("Thuc hien phep chia ma tran.");
-        return performOperation(a, "/");
+    Matrix(const Matrix& a) : m(a.m), n(a.n) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                elements[i][j] = a.elements[i][j];
+            }
+        }
     }
 
     void nhap() {
@@ -115,64 +59,93 @@ public:
         }
     }
 
-private:
-    MaTran performOperation(const MaTran& a, const string& operation) const {
-        MaTran result;
-        result.m = m;
-        result.n = n;
+    Matrix operator+(const Matrix& a) const {
+        Matrix result(*this);
+        if (m == a.m && n == a.n) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    result.elements[i][j] += a.elements[i][j];
+                }
+            }
+            Logger::log("phep cong ma tran",0);
+            return result;
+        }
+        Logger::log("Khong the cong 2 ma tran khong cung kich thuoc",2);
+        Logger::log("Vui long nhap lai ma tran",1);
+        return *this;
+    }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (operation == "+") {
-                    result.elements[i][j] = elements[i][j] + a.elements[i][j];
-                } else if (operation == "-") {
-                    result.elements[i][j] = elements[i][j] - a.elements[i][j];
-                } else if (operation == "*") {
-                    result.elements[i][j] = elements[i][j] * a.elements[i][j];
-                } else if (operation == "/") {
-                    if (a.elements[i][j] != 0) {
-                        result.elements[i][j] = elements[i][j] / a.elements[i][j];
-                    } else {
-                        // Xu ly chia cho 0
-                        GhiLog logger("logfile.txt");
-                        logger.ghiLoi("Loi: Chia cho 0!");
-                        result.elements[i][j] = 0; // hoac gan mot gia tri mac dinh khac
+    Matrix operator-(const Matrix& a) const {
+        Matrix result(*this);
+        if (m == a.m && n == a.n) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    result.elements[i][j] -= a.elements[i][j];
+                }
+            }
+            Logger::log("Phep tru ma tran",0);
+            return result;
+        }
+        Logger::log("Khong the tru 2 ma tran khong cung kich thuoc",2);
+        Logger::log("Vui long nhap lai ma tran",1);
+        return *this;
+    }
+
+    Matrix operator*(const Matrix& a) const {
+        Matrix result;
+        if (n == a.m) {
+            result.m = m;
+            result.n = a.n;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < a.n; j++) {
+                    result.elements[i][j] = 0;
+                    for (int k = 0; k < n; k++) {
+                        result.elements[i][j] += elements[i][k] * a.elements[k][j];
                     }
                 }
             }
+            Logger::log("Nhan ma tran",0);
+            return result;
         }
-
+        Logger::log("Khong nhan duoc 2 ma tran khac kich thuoc",2);
+        Logger::log("Vui long nhap lai ma tran",1);
         return result;
+    }
+
+    Matrix operator/(const Matrix& a) const {
+        Logger::log("Khong co phep chia",2);
+        Logger::log("Ban co the su dung ma tran nghich dao",1);
+        return *this;
     }
 };
 
 int main() {
-    MaTran mt1, mt2;
-    GhiLog logger("logfile.txt");
-
-    cout << "Nhap ma tran 1:" << endl;
+    Matrix mt1, mt2;
+    cout << "Nhap ma tran thu nhat:" << endl;
     mt1.nhap();
-
-    cout << "Nhap ma tran 2:" << endl;
+    cout << "Nhap ma tran thu hai:" << endl;
     mt2.nhap();
 
-    cout << "Ma tran 1:" << endl;
+    cout << "Ma tran thu nhat:" << endl;
     mt1.xuat();
-
-    cout << "Ma tran 2:" << endl;
+    cout << "Ma tran thu hai:" << endl;
     mt2.xuat();
 
-    cout << "Ket qua Cong:" << endl;
-    (mt1 + mt2).xuat();
+    Matrix result;
 
-    cout << "Ket qua Tru:" << endl;
-    (mt1 - mt2).xuat();
+    result = mt1 + mt2;
+    cout << "Ket qua phep cong ma tran:" << endl;
+    result.xuat();
 
-    cout << "Ket qua Nhan:" << endl;
-    (mt1 * mt2).xuat();
+    result = mt1 - mt2;
+    cout << "Ket qua phep tru ma tran:" << endl;
+    result.xuat();
 
-    cout << "Ket qua Chia:" << endl;
-    (mt1 / mt2).xuat();
+    result = mt1 * mt2;
+    cout << "Ket qua phep nhan ma tran:" << endl;
+    result.xuat();
 
+    result = mt1 / mt2;
+    cout << "Ket qua phep chia ma tran:" << endl;
     return 0;
 }
